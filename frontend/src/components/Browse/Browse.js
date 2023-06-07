56d642596c387d991217d3381a4a3aecafa7f23c
anishapoudel <77762595+anishapoudel@users.noreply.github.com>
2023-06-07 13:34:13 -0400

    Update Browse.js

--- frontend/src/components/Browse/Browse.js
+++ frontend/src/components/Browse/Browse.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-// import Browse from "./Allproducts.js";
-// import "./App.css";
-
-// import { useState } from "react";
+import Allproducts from "./Allproducts.js";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 function Browse() {
   const [triviaData, setTriviaData] = useState([]);
@@ -10,7 +15,7 @@ function Browse() {
   const [category, setCategory] = useState([]);
 
   const handleSubmit = () => {
-    fetch(`https://dummyjson.com/products/category/groceries`)
+    fetch(`https://dummyjson.com/products/category/${text}`)
       .then((response) => response.json())
       .then((data) => setTriviaData(data.products))
       .catch((error) => console.log("Error: ", error));
@@ -23,70 +28,37 @@ function Browse() {
   return (
     <div className="page">
       <header>
-        <div className="title">
-          <h1>Trivia Quiz</h1>
-        </div>
-        <label for="string">Number of Questions: </label>
+        <label for="string">Find anything you need: </label>
         <input
           id="string"
-          placeholder="Integer 1-50"
+          placeholder="Search for anything..."
           type="string"
           onChange={(event) => setText(event.target.value)}
         ></input>
         <div>
-          <div className="categories">
-            <p>Category List:</p>
-            <ol>1. General Knowledge</ol>
-            <ol>2. Books</ol>
-            <ol>3. Film</ol>
-            <ol>4. Music</ol>
-            <ol>5. Musical & Theatres</ol>
-            <ol>6. TV</ol>
-            <ol>7. Video Games</ol>
-            <ol>8. Board Games</ol>
-            <ol>9. Science & Nature</ol>
-            <ol>10. Computers </ol>
-          </div>
-
-          <label for="string"> Category: </label>
-          <input
-            id="string"
-            placeholder="Interger 1-10"
-            type="string"
-            onChange={(event) => setCategory(event.target.value)}
-          ></input>
           <button class="fa fa-search" type="submit" onClick={handleSubmit}>
             GO!
           </button>
         </div>
       </header>
 
-      {triviaData.length === 0 ? (
-        <p> No questions available at this time.</p>
-      ) : (
-        <div>
+      <Container maxWidth="lg">
+        <Grid container spacing={4} justify="left">
           {triviaData.map((item, index) => (
-            <Browse
-              key={index}
-              title={item.title}
-              description={item.description}
-              price={item.price}
-            />
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <Card style={{ height: "100%" }}>
+                <Allproducts
+                  title={item.title}
+                  description={item.description}
+                  price={item.price}
+                />
+              </Card>
+            </Grid>
           ))}
-        </div>
-      )}
-
-      <label for="string">More Questions? </label>
-      <input
-        id="string"
-        placeholder="Integer 1-50"
-        type="string"
-        onChange={(event) => setText(event.target.value)}
-      ></input>
-      <button class="fa fa-search" type="submit" onClick={handleSubmit}>
-        GO!
-      </button>
+        </Grid>
+      </Container>
     </div>
   );
 }
 export default Browse;
+
