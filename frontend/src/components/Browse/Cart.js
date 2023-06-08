e7a3b73b697aa8e6a855b20c46bb58d697b45080
anishapoudel <77762595+anishapoudel@users.noreply.github.com>
2023-06-08 08:39:22 -0400

    Update Cart.js

--- frontend/src/components/Browse/Cart.js
+++ frontend/src/components/Browse/Cart.js
@@ -1,3 +1,4 @@
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
@@ -5,41 +6,8 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { Container, Grid } from "@mui/material";
-import { QuantityPicker } from "react-qty-picker";
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 function Cart(props) {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -53,7 +21,7 @@ function Cart(props) {
   return (
     <div>
       <Button variant="" onClick={handleClickOpen}>
-        <AddCircleIcon />
+        <AddShoppingCartIcon />{" "}
       </Button>
       <Dialog
         open={open}
@@ -61,53 +29,23 @@ function Cart(props) {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <Container maxWidth="lg">
-          <Grid container spacing={3} justify="left">
-            <Grid item xs={1} sm={1} md={1} lg={3}>
-              <Card sx={{ minWidth: 345 }}>
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image="/static/images/cards/paella.jpg"
-                  alt={props.title}
-                />
-                <CardContent>
-                  <Typography variant="body2" color="text.secondary">
-                    ${props.price}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {props.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {props.rating}
-                  </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                  <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                  >
-                    <ExpandMoreIcon />
-                  </ExpandMore>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                  <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>{props.brand}</Typography>
-                    <Typography paragraph>{props.description}</Typography>
-                  </CardContent>
-                </Collapse>
-              </Card>{" "}
-            </Grid>
-            ))
-          </Grid>
-        </Container>
+        <DialogTitle id="alert-dialog-title">
+          {" Item successfully added to your cart"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {props.title}
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            {props.description}
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            Quantity: {props.quantity}
+          </DialogContentText>
+        </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
           <Button onClick={handleClose} autoFocus>
-            Confirm
+            Continue Shopping
           </Button>
         </DialogActions>
       </Dialog>
